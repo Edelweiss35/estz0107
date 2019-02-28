53d1c3b1e169b9604240aae28093c6ab3f860aa2
Edelweiss35 <Edelweiss35@outlook.com>
2019-02-28 02:53:22 -0800

    make it better

--- estz/site_utils.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-from django.template import RequestContext
-
-def error404(request):
-	return render(request,'errors/404.html')
--- plugin/cms_plugins.py
+++
@@ -1,9 +0,0 @@
-from cms.plugin_base import CMSPluginBase
-from cms.plugin_pool import plugin_pool
-from cms.models.pluginmodel import CMSPlugin
-from django.utils.translation import ugettext_lazy as _
-@plugin_pool.register_plugin
-class HelloPlugin(CMSPluginBase):
-    model = CMSPlugin
-    render_template = "index.html"
-    cache = False
